957e6e3c14eae676427807a5a715478c1ddc46a3
tt1990s <b1eb.7sq@gmail.com>
2022-11-03 16:40:19 +0900

    サンプルファイル追加

---
+++ data/like_sample.js
@@ -0,0 +1,16 @@
+window.YTD.like.part0 = [
+  {
+    "like" : {
+      "tweetId" : "XXXXXX",
+      "fullText" : "XXXXXX",
+      "expandedUrl" : "XXXXXX"
+    }
+  },
+  {
+    "like" : {
+      "tweetId" : "XXXXXX",
+      "fullText" : "XXXXXX",
+      "expandedUrl" : "XXXXXX"
+    }
+  }
+]
---
+++ data/tweets_sample.js
@@ -0,0 +1,72 @@
+window.YTD.tweets.part0 = [
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "XXXXX"
+          ],
+          "editableUntil" : "YYYYY-MM-DDTHH:MM:SS.000Z",
+          "editsRemaining" : "X",
+          "isEditEligible" : true
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://XXXXX/\" rel=\"nofollow\">XXXXX</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [ ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "X",
+        "X"
+      ],
+      "favorite_count" : "X",
+      "id_str" : "XXXXX",
+      "truncated" : false,
+      "retweet_count" : "X",
+      "id" : "XXXXX",
+      "created_at" : "Xxx Xxx DD HH:MM:SS +0000 YYYY",
+      "favorited" : false,
+      "full_text" : "XXXXX",
+      "lang" : "xx"
+    }
+  },
+  {
+    "tweet" : {
+      "edit_info" : {
+        "initial" : {
+          "editTweetIds" : [
+            "XXXXX"
+          ],
+          "editableUntil" : "YYYYY-MM-DDTHH:MM:SS.000Z",
+          "editsRemaining" : "X",
+          "isEditEligible" : true
+        }
+      },
+      "retweeted" : false,
+      "source" : "<a href=\"http://XXXXX/\" rel=\"nofollow\">XXXXX</a>",
+      "entities" : {
+        "hashtags" : [ ],
+        "symbols" : [ ],
+        "user_mentions" : [ ],
+        "urls" : [ ]
+      },
+      "display_text_range" : [
+        "X",
+        "X"
+      ],
+      "favorite_count" : "X",
+      "id_str" : "XXXXX",
+      "truncated" : false,
+      "retweet_count" : "X",
+      "id" : "XXXXX",
+      "created_at" : "Xxx Xxx DD HH:MM:SS +0000 YYYY",
+      "favorited" : false,
+      "full_text" : "XXXXX",
+      "lang" : "xx"
+    }
+  }
+]
